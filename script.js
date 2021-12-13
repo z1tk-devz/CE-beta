195d498e8c0568a5be850d97ac4a1d08de8ec8a6
ZYTK Devs <93052243+z1tk@users.noreply.github.com>
2021-12-13 09:05:49 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const left = document.querySelector(".left"),
 right = document.querySelector(".right"),
-bar = document.querySelector(".bar"),
 editorhtml = document.querySelector(".editor-html"),
 editorcss = document.querySelector(".editor-css"),
 editorjs = document.querySelector(".editor-js"),
@@ -9,82 +8,70 @@ tocl2 = document.querySelector(".tocl-2"),
 tocl3 = document.querySelector(".tocl-3"),
 run = document.querySelector(".btn-run"),
 iframe = document.querySelector(".iframe"),
-darkMode = document.querySelector(".btn-dark"),
-lightMode = document.querySelector(".btn-light"),
+darkLightMode = document.querySelector(".btn-dark-light"),
 menu = document.querySelector(".editor-menu"),
 live = document.querySelector(".live"),
 livebtn = document.querySelector("#live"),
 body = document.querySelector(".body");
 
-
-const drag = (e) => {
-    e.preventDefault();
-    document.selection ? document.selection.empty() : window.getSelection().removeAllRanges();
-    left.style.width = (e.pageX - bar.offsetWidth / 3) + "px";
-    editor.resize();
-}
-
-bar.addEventListener("mousedown", () => {
-    document.addEventListener("mousemove", drag);
-})
-
-bar.addEventListener("mouseup", () => {
-    document.removeEventListener("mousemove", drag);
-})
+let ldm = 0;
 
 // Run Btn Event Listener
 run.addEventListener("click", runcode)
 
 // Set Dark Mode
-darkMode.addEventListener("click", () => {
-    editorhtml.style.backgroundColor = "#363836";
-    editorhtml.style.color = "#eee";
-    editorcss.style.backgroundColor = "#363836";
-    editorcss.style.color = "#eee";
-    editorjs.style.backgroundColor = "#363836";
-    editorjs.style.color = "#eee";
-    tocl1.style.backgroundColor = "darkslategray";
-    tocl1.style.color = "#eee";
-    tocl2.style.backgroundColor = "darkslategray";
-    tocl2.style.color = "#eee";    
-    tocl3.style.backgroundColor = "darkslategray";
-    tocl3.style.color = "#eee";
-    menu.style.backgroundColor = "#363836";    
-    body.style.backgroundColor = "darkslategray";
-    darkMode.style.backgroundColor = "darkslategray";
-    lightMode.style.backgroundColor = "darkslategray";
-    live.style.backgroundColor = "darkslategray";
-    run.style.backgroundColor = "darkslategray";
-    darkMode.style.color = "#eee";
-    lightMode.style.color = "#eee";
-    run.style.color = "#eee";
-    live.style.color = "#eee";
-})
+darkLightMode.addEventListener("click", () => {
+    
+    if (ldm == '0') {
+        ldm = 1;
+        darkLightMode.innerHTML = "Light Mode";
+        editorhtml.style.backgroundColor = "#363836";
+        editorhtml.style.color = "#eee";
+        editorcss.style.backgroundColor = "#363836";
+        editorcss.style.color = "#eee";
+        editorjs.style.backgroundColor = "#363836";
+        editorjs.style.color = "#eee";
+        tocl1.style.backgroundColor = "darkslategray";
+        tocl1.style.color = "#eee";
+        tocl2.style.backgroundColor = "darkslategray";
+        tocl2.style.color = "#eee";    
+        tocl3.style.backgroundColor = "darkslategray";
+        tocl3.style.color = "#eee";
+        menu.style.backgroundColor = "#363836";    
+        body.style.backgroundColor = "darkslategray";
+        darkLightMode.style.backgroundColor = "darkslategray";
+        live.style.backgroundColor = "darkslategray";
+        run.style.backgroundColor = "darkslategray";
+        darkMode.style.color = "#eee";
+        lightMode.style.color = "#eee";
+        run.style.color = "#eee";
+        live.style.color = "#eee";
+    } else if (ldm == '1') {
+        ldm = 0;
+        darkLightMode.innerHTML = "Dark Mode";
+        editorhtml.style.backgroundColor = "";
+        editorhtml.style.color = "";
+        editorcss.style.backgroundColor = "";
+        editorcss.style.color = "";    
+        editorjs.style.backgroundColor = "";
+        editorjs.style.color = "";
+        tocl1.style.backgroundColor = "lightgray";
+        tocl1.style.color = "";
+        tocl2.style.backgroundColor = "lightgray";
+        tocl2.style.color = "";
+        tocl3.style.backgroundColor = "lightgray";
+        tocl3.style.color = "";
+        body.style.backgroundColor = "";
+        menu.style.backgroundColor = "";
+        darkLightMode.style.backgroundColor = "#31ad26";
+        live.style.backgroundColor = "#31ad26";
+        run.style.backgroundColor = "#31ad26";
+        darkMode.style.color = "#fff";
+        lightMode.style.color = "#fff"
+        run.style.color = "#fff";
+        live.style.color = "";
+    }
 
-// Set Light Mode
-lightMode.addEventListener("click", () => {
-    editorhtml.style.backgroundColor = "";
-    editorhtml.style.color = "";
-    editorcss.style.backgroundColor = "";
-    editorcss.style.color = "";    
-    editorjs.style.backgroundColor = "";
-    editorjs.style.color = "";
-    tocl1.style.backgroundColor = "lightgray";
-    tocl1.style.color = "";
-    tocl2.style.backgroundColor = "lightgray";
-    tocl2.style.color = "";
-    tocl3.style.backgroundColor = "lightgray";
-    tocl3.style.color = "";
-    body.style.backgroundColor = "";
-    menu.style.backgroundColor = "";
-    darkMode.style.backgroundColor = "#31ad26";
-    lightMode.style.backgroundColor = "#31ad26";
-    live.style.backgroundColor = "#31ad26";
-    run.style.backgroundColor = "#31ad26";
-    darkMode.style.color = "#fff";
-    lightMode.style.color = "#fff"
-    run.style.color = "#fff";
-    live.style.color = "";
 })
 
 // LIVE CODE
